4ab644546cc69ccef5358f2f2d4b2a1b8cf55ae1
Enrique Hilst <enrique.hilst@gmail.com>
2020-06-28 23:48:16 -0500

    complete css and JS

--- js/appNonProto.js
+++ js/appNonProto.js
@@ -1,6 +1,8 @@
 'use strict';
 
 
+
+
 ///////// Lab 06 below /////////
 var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 
@@ -280,3 +282,5 @@ Uncomment below to display data by non-proto method
 //   }
 // }
 // // renderAllShops();
+
+
